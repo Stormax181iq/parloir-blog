1791bde986cd05c8181df4c30286a90742e663eb
Stormax181iq <kelianchalier@gmail.com>
2024-11-09 11:52:31 +0100

    home page img styling and html

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,16 @@
+import homeImg from "../assets/homeWomanArt.png";
+
 export default function Home() {
-  return <h1>Home !</h1>;
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <h1>Let your stories shine.</h1>
+      <p>Parloir helps others read, write, and deepen their understanding.</p>
+      <button>Start reading</button>
+      <img
+        src={homeImg}
+        alt="Womens Day Flower Abstract Art"
+        className="absolute -right-40 h-5/6 -translate-y-8 object-cover"
+      />
+    </div>
+  );
 }
