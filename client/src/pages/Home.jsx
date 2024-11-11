e2b824f4b744ac89acfd28b8c4b583d254c125a1
Stormax181iq <kelianchalier@gmail.com>
2024-11-11 13:00:25 +0100

    most popular and recent posts design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,8 +29,8 @@ export default function Home() {
           className="absolute -right-40 h-full object-cover"
         />
       </div>
-      <div className="flex justify-between mx-64 px-16 my-4">
-        <div className="w-2/3">
+      <div className="flex justify-stretch mx-64 px-16 my-4">
+        <div className="w-2/3 mr-4">
           <h1 ref={popularRef} className="text-3xl font-h mb-4">
             Popular categories
           </h1>
@@ -73,24 +73,220 @@ export default function Home() {
             </a>
           </div>
           <h1 className="text-3xl font-h my-4">Recent posts</h1>
-          <div className="flex">
-            <img src="https://picsum.photos/300/200" alt="" />
-            <div className="m-4">
+          <div className="flex mb-8 pb-8 border-b border-main-black/10">
+            <img src="https://picsum.photos/300/201" alt="" />
+            <div className="flex flex-col m-4 justify-between">
               <p>03/08/2024 19:12 &#x00B7; Coding</p>
               <p>Emelin Miller</p>
               <h2 className="text-2xl font-h">Will Deno2 overtake Node.js ?</h2>
               <p className="mb-4">
                 For years, we’ve been using Node.js for the backend …
               </p>
-              <MainButton isLink={true} link="#" className="py-2 px-3">
+              <MainButton
+                isLink={true}
+                link="#"
+                className="py-2 px-3 w-28 text-center"
+              >
+                Read More
+              </MainButton>
+            </div>
+          </div>
+          <div className="flex mb-8 pb-8 border-b border-main-black/10">
+            <img src="https://picsum.photos/300/199" alt="" />
+            <div className="flex flex-col m-4 justify-between">
+              <p>12/12/2024 12:12 &#x00B7; Productivity</p>
+              <p>Dan</p>
+              <h2 className="text-2xl font-h">
+                How to type fast (100 words per minute)
+              </h2>
+              <p className="mb-4">
+                I spent too much time trying to improve my typing speed by doing
+                the wrong things …
+              </p>
+              <MainButton
+                isLink={true}
+                link="#"
+                className="py-2 px-3 w-28 text-center"
+              >
+                Read More
+              </MainButton>
+            </div>
+          </div>
+          <div className="flex mb-8 pb-8 border-b border-main-black/10">
+            <img src="https://picsum.photos/300/198" alt="" />
+            <div className="flex flex-col m-4 justify-between">
+              <p>23/09/2024 22:02 &#x00B7; Travel</p>
+              <p>Donald Smith</p>
+              <h2 className="text-2xl font-h">My journey in Zagreb</h2>
+              <p className="mb-4">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+                dolorem? Dolorem, ullam quod. Sint, accusantium …
+              </p>
+              <MainButton
+                isLink={true}
+                link="#"
+                className="py-2 px-3 w-28 text-center"
+              >
+                Read More
+              </MainButton>
+            </div>
+          </div>
+          <div className="flex">
+            <img src="https://picsum.photos/300/197" alt="" />
+            <div className="flex flex-col m-4 justify-between">
+              <p>22/09/2024 02:34 &#x00B7; Coding</p>
+              <p>Arnoldlebg</p>
+              <h2 className="text-2xl font-h">
+                Creating my own database system, from scratch!
+              </h2>
+              <p className="mb-4">
+                I realised that some aspect of the current databases are bad, so
+                I decided to create one myself …
+              </p>
+              <MainButton
+                isLink={true}
+                link="#"
+                className="py-2 px-3 w-28 text-center"
+              >
                 Read More
               </MainButton>
             </div>
           </div>
         </div>
-        <div>
-          <h1 className="text-3xl font-h">Most popular</h1>
-          <h1 className="text-3xl font-h">Editor’s choice</h1>
+        <div className="ml-8 pl-8 border-l border-main-black">
+          <h1 className="text-3xl font-h mb-4">Most popular</h1>
+          <div className="mb-6">
+            <a
+              href="/posts/categories/travel"
+              className="bg-accent py-1 px-2 rounded-full"
+            >
+              Travel
+            </a>
+            <a href="/users/john-doe/posts/a-journey-in-ales-194834">
+              <h2 className="text-xl font-h underline mt-1">
+                A journey in Alès
+              </h2>
+            </a>
+            <p>John Doe &#x00B7; 27/07/2024</p>
+          </div>
+          <div className="mb-6">
+            <a
+              href="/posts/categories/travel"
+              className="bg-accent py-1 px-2 rounded-full"
+            >
+              Travel
+            </a>
+            <a href="/users/john-doe/posts/a-journey-in-ales-194834">
+              <h2 className="text-xl font-h underline mt-1">
+                A journey in Alès
+              </h2>
+            </a>
+            <p>John Doe &#x00B7; 27/07/2024</p>
+          </div>
+          <div className="mb-6">
+            <a
+              href="/posts/categories/travel"
+              className="bg-accent py-1 px-2 rounded-full"
+            >
+              Travel
+            </a>
+            <a href="/users/john-doe/posts/a-journey-in-ales-194834">
+              <h2 className="text-xl font-h underline mt-1">
+                A journey in Alès
+              </h2>
+            </a>
+            <p>John Doe &#x00B7; 27/07/2024</p>
+          </div>
+          <div className="mb-6">
+            <a
+              href="/posts/categories/travel"
+              className="bg-accent py-1 px-2 rounded-full"
+            >
+              Travel
+            </a>
+            <a href="/users/john-doe/posts/a-journey-in-ales-194834">
+              <h2 className="text-xl font-h underline mt-1">
+                A journey in Alès
+              </h2>
+            </a>
+            <p>John Doe &#x00B7; 27/07/2024</p>
+          </div>
+          <div className="mb-6">
+            <a
+              href="/posts/categories/travel"
+              className="bg-accent py-1 px-2 rounded-full"
+            >
+              Travel
+            </a>
+            <a href="/users/john-doe/posts/a-journey-in-ales-194834">
+              <h2 className="text-xl font-h underline mt-1">
+                A journey in Alès
+              </h2>
+            </a>
+            <p>John Doe &#x00B7; 27/07/2024</p>
+          </div>
+
+          <h1 className="text-3xl font-h my-4">Editor’s choice</h1>
+          <div className="my-8">
+            <a
+              href="/posts/categories/sport"
+              className="bg-accent py-1 px-2 rounded-full ml-14"
+            >
+              Sport
+            </a>
+            <div className="flex my-2 items-center">
+              <img
+                src="https://picsum.photos/50/50"
+                alt=""
+                className="rounded-full mt-1"
+              />
+              <a href="/users/runner-passion/posts/how-i-broke-20-on-the-5k-439203">
+                <h2 className="mx-2 font-h text-xl underline">
+                  How I broke 20’ on the 5k
+                </h2>
+              </a>
+            </div>
+          </div>
+          <div className="my-8">
+            <a
+              href="/posts/categories/fashion"
+              className="bg-accent py-1 px-2 rounded-full ml-14"
+            >
+              Fashion
+            </a>
+            <div className="flex my-2 items-center">
+              <img
+                src="https://picsum.photos/50/49"
+                alt=""
+                className="rounded-full mt-1"
+              />
+              <a href="/users/billiejogging/posts/how-to-have-style-184343">
+                <h2 className="mx-2 font-h text-xl underline">
+                  How to have style
+                </h2>
+              </a>
+            </div>
+          </div>
+          <div className="my-8">
+            <a
+              href="/posts/categories/music"
+              className="bg-accent py-1 px-2 rounded-full ml-14"
+            >
+              Music
+            </a>
+            <div className="flex my-2 items-center">
+              <img
+                src="https://picsum.photos/49/50"
+                alt=""
+                className="rounded-full mt-1"
+              />
+              <a href="/users/saratte/posts/play-the-piano-better-593920">
+                <h2 className="mx-2 font-h text-xl underline">
+                  Play the piano better
+                </h2>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
